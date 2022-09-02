297a057754a6506a668abb3c050abe241096d379
Laurent LE LARDEUX <laurent.le-lardeux@inetum.com>
2022-09-02 17:41:42 +0200

    template literals example

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import './App.css';
-import { RandomNumber } from './components/restriction/RandomNumber';
+import { Toast } from './components/templateliterals/Toast';
 
 function App() {
   return (
     <div className="App">
-      <RandomNumber value={10} isPositive={true} />
+      <Toast position='left-center' />
     </div>
   );
 }
