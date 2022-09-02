4ba32f5a9441b182e6783141eeb1bc915ee63844
Laurent LE LARDEUX <laurent.le-lardeux@inetum.com>
2022-09-02 13:58:28 +0200

    more event as component props

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css';
 import { Button } from './components/Button';
+import { Input } from './components/Input';
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <Button handleClick={(event, id) => {
         console.log('button clicked', event, id)
       }}/>
+      <Input value='' handleChange={(event) => console.log(event)} />
     </div>
   );
 }
