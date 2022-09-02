b2196c5dcba4c0b14213662c1a4d33fc11c3cbfe
Laurent LE LARDEUX <laurent.le-lardeux@inetum.com>
2022-09-02 17:04:34 +0200

    generic props example

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
 import './App.css';
-import { Private } from './components/auth/Private';
-import { Profile } from './components/auth/Profile'
+import { List } from './components/generics/List';
 
 function App() {
   return (
     <div className="App">
-      <Private isLoggedIn={true} component={Profile} />
+{/*       <List
+        items={['Batman', 'Superman', 'Spiderman']}
+        onClick={(item) => console.log(item)}
+      />
+      <List
+        items={[1, 2, 3]}
+        onClick={(item) => console.log(item)}
+      /> */}
+      <List
+        items={[
+          {
+            id: 1,
+            first: 'Bruce',
+            last: 'Wayne'
+          },
+          {
+            id: 2,
+            first: 'Clark',
+            last: 'Kent'
+          },
+          {
+            id: 3,
+            first: 'Dave',
+            last: 'Appadoo'
+          },
+        ]}
+        onClick={(item) => console.log(item)}
+      />
     </div>
   );
 }
